c68436c941dbb80fb5753e160bf4b614ee09827d
Igor Kruchynskyi <kruchyns@adobe.com>
2024-03-06 16:07:28 +0000

    testing codespaces

--- detector/emotionAnalyzer.py
+++ detector/emotionAnalyzer.py
@@ -7,7 +7,7 @@ from transformers import logging as hf_logging
 #Disable logging
 hf_logging.set_verbosity_error()
 
-emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+
 def analyze(text):
     text = normalize(text)
     return emotion(text)
@@ -15,3 +15,9 @@ def analyze(text):
 def normalize(text):
     return "".join(ch for ch in text if ch.isalnum() or ch == " ")
 
+def main(*argc):
+    emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+    emotion(argc[1])
+
+if __name__ == "__main__":
+    main(*sys.argv)
